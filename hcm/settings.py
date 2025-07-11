671b11b502c49fc504b16e8e52e3aee8e6623005
Josué <126045947+JosueEUsecheCh@users.noreply.github.com>
2025-07-11 13:45:04 -0400

    Actualizar settings y requirements

--- hcm/settings.py
+++ hcm/settings.py
@@ -133,10 +133,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # carpeta para collectstatic
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'consultas/static'),
+    os.path.join(BASE_DIR, 'consultas/static'),  # carpeta con tus archivos estáticos de la app
 ]
 
 # Default primary key field type
